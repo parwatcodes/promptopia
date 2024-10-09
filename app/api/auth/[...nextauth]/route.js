48d848ad1ed61574190785fd6afed54aa03cf202
Parwat Kunwar <parwatkunwar08@gmail.com>
2024-10-08 23:27:39 -0300

    Fix nextauth callback handlers for session and sign

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google';
 
 import { connectToDB } from "@utils/database";
 import User from "@models/User";
-
-console.log('xx', process.env.GOOGLE_ID)
 
 const handler = NextAuth({
   providers: [
@@ -13,30 +11,41 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-  async session({ session }) {
+  callback: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
+      });
 
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+      session.user.id = sessionUser._id.toString();
 
-      // check if a user already exists.
+      return session;
+    },
 
-      const userExists = await User.findOne({ email: profile.email })
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture
-        })
+        const userExists = await User.findOne({ email: profile.email });
+
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+          });
+        }
+
+        return true;
+
+        // if not create a user and save it to the database.
+      } catch (error) {
+        console.log(error);
+
+        return false;
       }
-
-      // if not create a user and save it to the database.
-    } catch (error) {
-
     }
   }
-})
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
